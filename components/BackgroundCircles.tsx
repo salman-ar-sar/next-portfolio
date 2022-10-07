8997b5c23a73df1caff519ef300f1f748de82c30
Salman A <salman.a@hatio.in>
2022-10-07 14:15:53 +0530

    Add Landing component

---
+++ components/BackgroundCircles.tsx
@@ -0,0 +1,26 @@
+import { motion } from "framer-motion";
+
+export default function BackgroundCircles() {
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        borderRadius: ["20%", "20%", "50%", "80%", "20%"],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1],
+        scale: [1, 2, 2, 3, 1],
+      }}
+      transition={{
+        duration: 2.5,
+      }}
+      className="relative flex justify-center items-center"
+    >
+      <div className="absolute border border-cyan-900 opacity-50 rounded-full h-[12rem] w-[12rem] mt-52 animate-ping" />
+      <div className="absolute border border-cyan-900 opacity-50 rounded-full h-[20rem] w-[20rem] mt-52" />
+      <div className="absolute border border-cyan-900 opacity-50 rounded-full h-[28rem] w-[28rem] mt-52" />
+      <div className="absolute border border-cyan-400 opacity-20 rounded-full h-[36rem] w-[36rem] mt-52 animate-pulse" />
+      <div className="absolute border border-cyan-900 opacity-50 rounded-full h-[44rem] w-[44rem] mt-52" />
+    </motion.div>
+  );
+}
---
+++ components/Landing.tsx
@@ -0,0 +1,53 @@
+import Image from "next/future/image";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import ProfilePicture from "../assets/ProfilePicture.jpg";
+import Link from "next/link";
+
+type LandingProps = {};
+
+export default function Landing({}: LandingProps) {
+  const [text, count] = useTypewriter({
+    words: ["Hey, Salman here", "<TechNerd />", "GeekyPerfectionist.tsx"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <section
+      id="landing"
+      className="h-screen snap-center flex flex-col items-center justify-center text-center overflow-hidden space-y-8"
+    >
+      <BackgroundCircles />
+      <Image
+        className="relative rounded-full h-32 w-32 object-cover"
+        src={ProfilePicture}
+        alt="My profile picture"
+      />
+      <div className="z-10">
+        <h2 className="text-lg uppercase text-gray-500 pb-2 tracking-[0.75rem]">
+          Full Stack Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="cyan" />
+        </h1>
+
+        <div className="mt-5">
+          <Link href="#about">
+            <button className="landing-button">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="landing-button">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="landing-button">Skills</button>
+          </Link>
+          <Link href="#works">
+            <button className="landing-button">Works</button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
