ec8468d8e3e52754724d39f43da4328a698424bc
Salman A <salman.a@hatio.in>
2022-10-30 00:28:18 +0530

    Add service fns

---
+++ services/index.ts
@@ -0,0 +1,57 @@
+import type {
+  Experience,
+  PageInfo,
+  Project,
+  Social,
+  Technology,
+} from "../shared/types";
+
+export const fetchPageInfo = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pageInfo`);
+
+  const data = await res.json();
+
+  const pageInfo: PageInfo = data.pageInfo;
+
+  return pageInfo;
+};
+
+export const fetchSocials = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/socials`);
+
+  const data = await res.json();
+
+  const socials: Social[] = data.socials;
+
+  return socials;
+};
+
+export const fetchTechnologies = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/skills`);
+
+  const data = await res.json();
+
+  const technologies: Technology[] = data.technologies;
+
+  return technologies;
+};
+
+export const fetchProjects = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projects`);
+
+  const data = await res.json();
+
+  const projects: Project[] = data.projects;
+
+  return projects;
+};
+
+export const fetchExperiences = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/experience`);
+
+  const data = await res.json();
+
+  const experiences: Experience[] = data.experiences;
+
+  return experiences;
+};
